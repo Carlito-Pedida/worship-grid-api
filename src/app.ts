28e0cb7bda0645af1f5660da2c7b8b30139485c8
ionic-project <carlped417@gmail.com>
2024-02-03 10:49:23 -0600

    Stripe Payment Integration

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,11 @@ const corsOptions = {
   origin: ["http://localhost:4200", "http://localhost:5001"]
 };
 
+const stripe = require("stripe")(process.env.STRIPE_SK_CODE);
+
 app.use(cors(corsOptions));
+app.use(express.static("public"));
+app.use(express.json());
 app.use(morgan("dev"));
 
 app.use(express.json());
@@ -22,13 +26,38 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/server/users", userRoutes);
 app.use("/server/assets", assetRoutes);
 app.use("/server/responses", responseRoutes);
+app.post("/server/checkout", async (req, res) => {
+  console.log("Received checkout request");
+  console.log(req.body);
+  const items = req.body.items;
+  let lineItems: any = [];
+  items.forEach((item: any) => {
+    lineItems.push({
+      price: item.merch_id,
+      quantity: item.qty
+    });
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    mode: "payment",
+    success_url: "http://localhost:5001/paysuccessful",
+    cancel_url: "http://localhost:5001/paycanceled"
+  });
+
+  res.send(
+    JSON.stringify({
+      url: session.url
+    })
+  );
+});
 
 app.use((req, res, next) => {
   res.status(404).end();
 });
 
 // Syncing our database
-db.sync({ alter: false }).then(() => {
+db.sync({ alter: true }).then(() => {
   console.info("connected to the database!");
 });
 
