a0b0aa25dc2e9471b9ec069a6922d380883fb1ac
ionic-project <carlped417@gmail.com>
2024-07-13 14:36:32 -0500

    wg-api-build

--- build/messageRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const messages_1 = require("./messages");
-const router = (0, express_1.Router)();
-router.get("/", messages_1.displayForm);
-router.post("/", messages_1.processForm);
-exports.default = router;
