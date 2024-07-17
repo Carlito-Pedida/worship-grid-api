0f1324ab03518db60729283cc866759537e55c46
Node-Prophet <85714933+Carlito-Pedida@users.noreply.github.com>
2024-07-17 18:14:06 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || "5000";
 const corsOptions = {
-  origin: ["https://worship-grid-ui-production.vercel.app"]
+  origin: ["http://localhost:5001", "https://worship-grid-ui-production.vercel.app"]
 };
 
 app.get("/", (_req: Request, res: Response) => {
